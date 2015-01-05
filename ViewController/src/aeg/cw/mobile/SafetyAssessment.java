55819a34a781cfe9bd7488391be30e8e49a5c631
brito <francisco.brito@gmail.com>
2015-01-05 15:13:52 -0800

    Safety Assessment tasks #content

--- ViewController/src/aeg/cw/mobile/SafetyAssessment.java
+++ ViewController/src/aeg/cw/mobile/SafetyAssessment.java
@@ -4,5 +4,14 @@ public class SafetyAssessment extends Case {
     public SafetyAssessment(){
         this.setType("Safety Assessment");
         this.setPriority(Priority.Important);
+        
+        this.addTask("Safety Restrictions","Are safety threats being sufficiently managed in the least restrictive way possible?");
+        this.addTask("Find Strengths","Can existing protective capacities (strengths) be built upon to make needed changes?");
+        this.addTask("Make a Plan","What is the relationship between identified safety threats and currently diminished protective capacities?");
+        this.addTask("Same Page","What is the perspective or awareness regarding safety threats and their relationship to diminished parental protective capacities?");
+        this.addTask("Assign Tasks","What are parents ready, willing and able to do to make needed changes?");
+        this.addTask("Identify Challenges","What are the areas of disagreement between the parents and Child Welfare regarding what needs to change?");
+        this.addTask("Stick to the Plan","What change actions, services and activities will be used to assist in enhancing diminished parental protective capacities?");
+        
     }
 }
